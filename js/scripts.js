90a39d2e5cd5ffbc533f79e690db3cc56dd17c34
zwartapps <zwart.apps@gmail.com>
2020-10-13 21:06:06 +0100

    cards added and responsive navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
-const toTop = document.querySelector(".to-top");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
+//Auto close hamburger menu mobile
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var click = $(event.target);
+        var _open = $(".navbar-collapse").hasClass("show");
+        if (_open === true && !click.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+  });
+  
+  // Add active class to the current button (highlight it)
+  var header = document.getElementById("myNavbar");
+  var lis = header.getElementsByClassName("nav-item");
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+    });
   }
-})
-//test
